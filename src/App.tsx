304628338e9f324701d5924dccdc9a1a751927cb
sanz bajracharya <sanjay.bajracharya4@gmail.com>
2023-08-03 16:07:11 +1000

    Add button component using react-icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,30 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
 import Button from "./components/Button";
+import { FaBeer } from 'react-icons/fa';
+import {AiOutlineDownload} from 'react-icons/ai';
 
 function App() {
   const [count, setCount] = useState(0);
 
-  const handleButtonClick = () => {
-    alert("sanjay");
+  const handlePrimaryButton = () => {
+    alert("primary button click");
+  };
+
+  const handleSecondaryButton = () => {
+    alert("secondary button click");
+  };
+
+  const handleDangerButton = () => {
+    alert("danger button click");
   };
 
   return (
     <div className="App">
-      <h2> Test React </h2>
-      <Button onButtonClick={handleButtonClick}><span> i </span>Sanjay</Button>
+      <h2> React Button Component </h2>
+      <Button color="primary" onButtonClick={handlePrimaryButton}> <FaBeer /> Primary </Button>
+      <Button color="secondary" onButtonClick={handleSecondaryButton}> <AiOutlineDownload /> Secondary </Button>
+      <Button color="danger" onButtonClick={handleDangerButton}> Danger </Button>
     </div>
   );
 }
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,10 +3,11 @@ import React, { ReactNode } from "react";
 interface Props {
   children: ReactNode;
   onButtonClick: () => void;
+  color?: 'primary' | 'secondary' | 'danger'
 }
 
-const Button = ({ children, onButtonClick }: Props) => {
-  return <button onClick={onButtonClick}>{children}</button>;
+const Button = ({ children, onButtonClick, color='primary' }: Props) => {
+  return <button className={'mx-2 btn btn-'+color} onClick={onButtonClick}>{children}</button>;
 };
 
 export default Button;
